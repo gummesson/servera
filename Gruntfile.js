865ed5f8809ad59c4071ddff44ee63f437441f89
Ellen Gummesson <ellengummesson@gmail.com>
2013-10-27 15:45:23 +0100

    Add site files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,69 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  var project = {
+    js: {
       files: [
         'Gruntfile.js',
         'bin/*',
         'lib/*'
       ]
+    },
+
+    sass: {
+      src: 'assets/sass/style.scss',
+      dest: 'assets/css/style.css'
+    },
+
+    site: {
+      build: 'geno',
+      src: 'site/',
+      dest: '../gh-pages/'
+    }
+  };
+
+  grunt.initConfig({
+    project: project,
+
+    jshint: {
+      files: project.js.files
+    },
+
+    sass: {
+      build: {
+        options: {
+          style: 'compressed'
+        },
+
+        files: {
+          '<%= project.sass.dest %>' : '<%= project.sass.src %>'
+        }
+      }
+    },
+
+    shell: {
+      site: {
+        options: {
+          stdout: true
+        },
+        command: project.site.build
+      }
+    },
+
+    copy: {
+      site: {
+        files: [{
+          expand: true,
+          cwd: project.site.src,
+          src: ['**'],
+          dest: project.site.dest
+        }]
+      }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('site', ['sass', 'shell', 'copy']);
+
 };
